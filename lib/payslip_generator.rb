9d0d7937e6e6ab91ae262df97629ade308a454ba
Michael Treacher <m.w.treacher@gmail.com>
2016-11-14 15:06:21 +0100

    Add payslip factory for creating payslip hashes

--- lib/payslip_generator.rb
+++ lib/payslip_generator.rb
@@ -1,7 +1,8 @@
 require "bigdecimal"
+
 require "payslip_generator/tax_bracket"
 require "payslip_generator/tax_calculator"
-require "payslip_generator/net_income_calculator"
+require "payslip_generator/payslip_factory"
 
 module PayslipGenerator
 end
---
+++ spec/payslip_generator/payslip_factory_spec.rb
@@ -0,0 +1,39 @@
+describe PayslipGenerator::PayslipFactory do
+  let(:employee_attrs) do
+    {
+      first_name: "Michael",
+      last_name: "Treacher",
+      salary: BigDecimal.new("150000"),
+      super_rate: 0.09,
+      pay_period: "01 March - 31 March"
+    }
+  end
+
+  let(:tax) { BigDecimal.new("50000") }
+
+  let(:tax_calculator) { double(:tax_calculator, calculate: tax) }
+  let(:expected_payslip) do
+    {
+      first_name: "Michael",
+      last_name: "Treacher",
+      pay_period: "01 March - 31 March",
+      gross_income: 125_00,
+      income_tax: 4167,
+      net_income: 8333,
+      superannuation: 750
+    }
+  end
+
+  subject do
+    described_class.create(
+      employee_attrs: employee_attrs,
+      tax_calculator: tax_calculator
+    )
+  end
+
+  describe "#generate" do
+    it "returns a payslip hash" do
+      expect(subject).to eq expected_payslip
+    end
+  end
+end
