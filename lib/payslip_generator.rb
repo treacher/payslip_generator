4a3515a7907a3fca235e43395e45f068ed35692b
Michael Treacher <m.w.treacher@gmail.com>
2017-01-03 12:13:18 +0100

    Add requires for new dependencies

--- lib/payslip_generator.rb
+++ lib/payslip_generator.rb
@@ -1,10 +1,13 @@
 require "bigdecimal"
 require "yaml"
+require "ccsv"
 
 require "payslip_generator/tax_bracket"
 require "payslip_generator/tax_calculator"
+require "payslip_generator/runner"
 require "payslip_generator/factories/payslip"
 require "payslip_generator/factories/tax_bracket"
+require "payslip_generator/employee_readers/csv_reader"
 
 module PayslipGenerator
 end
